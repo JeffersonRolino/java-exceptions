39da1734592805b5e7e8c6fbf0a8c9318cdd8f01
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-20 11:34:34 -0300

    Capítulo 05 - Aula 03: Isolando a resposta de erro

--- src/main/java/adopet/api/exception/GlobalExceptionHandler.java
+++ src/main/java/adopet/api/exception/GlobalExceptionHandler.java
@@ -5,11 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AdocaoException.class)
-    public ResponseEntity<String> adocaoException(AdocaoException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<ResponseError> adocaoException(AdocaoException exception){
+
+        ResponseError responseError = new ResponseError(
+                exception.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseError);
     }
 }
