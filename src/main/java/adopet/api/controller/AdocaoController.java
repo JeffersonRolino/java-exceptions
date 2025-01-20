a8e4add4e040e76bb05790f29a6886477b757653
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-20 11:28:47 -0300

    Capítulo 05 - Aula 02: Criando um Controller Advice

--- src/main/java/adopet/api/controller/AdocaoController.java
+++ src/main/java/adopet/api/controller/AdocaoController.java
@@ -38,13 +38,7 @@ public class AdocaoController {
     @PostMapping
     @Transactional
     public ResponseEntity<String> solicitar(@RequestBody @Valid SolicitacaoDeAdocaoDTO dados){
-
-        try {
-            this.service.solicitar(dados);
-        } catch (AdocaoException exception){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
-        }
-
+        this.service.solicitar(dados);
         return ResponseEntity.ok("Adoção solicitada com sucesso!");
     }
 
